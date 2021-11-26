9b28d69b306ed3cd445daf2de1359fa465223e6b
TitoRaton <95055714+TitoRaton@users.noreply.github.com>
2021-11-26 12:05:51 +0100

    Add files via upload

---
+++ otroArchivoModificado.cs
@@ -0,0 +1,26 @@
+/*
+ *
+ *   Autor: Antonio Conesa
+ *
+ *   Programa que sirve para realizar el ejercicio de entornos
+ *   de desarrollo. Este ejercicio será el modificado.
+ *
+ */
+
+using System;
+
+class EjercicioInicial
+{
+   static void Main()
+   {
+      Console.WriteLine("**************************************************");
+      Console.WriteLine("**                                              **");
+      Console.WriteLine("**            Entornos de desarrollo            **");
+      Console.WriteLine("**                                              **");
+      Console.WriteLine("**   Este es el ejercicio inicial modificado    **");
+      Console.WriteLine("**                                              **");
+      Console.WriteLine("**   !!!!!!  - Archivo modificado -  !!!!!!!    **");
+      Console.WriteLine("**                                              **");
+      Console.WriteLine("**************************************************");
+   }
+}
